a8d7f97eddf014d7d79c50874eb27d582161392d
Yaroslav <yaroslavkopylov98@gmail.com>
2022-09-30 16:00:55 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", createImageCarts(galleryItems));
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    closeText: "x",
+    showCounter: false,
+});
+
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+});
+
+function createImageCarts(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>`;
+        })
+        .join("");
+}
