a77357447fd25127b3512e01ccd9174300a672cd
Yaroslav <yaroslavkopylov98@gmail.com>
2022-10-02 14:33:10 +0200

    finaly

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
-import vimeo from "@vimeo/player";
+import Player from "@vimeo/player";
+const throttle = require("lodash.throttle");
+
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+function onLoadTime(e) {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
+}
+
+player
+    .setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    .then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+        switch (error.name) {
+            case "RangeError":
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+
+player.on("timeupdate", throttle(onLoadTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const throttle = require("lodash.throttle");
 
+const form = document.querySelector(".feedback-form"),
+    email = document.querySelector("[type=email]"),
+    message = document.querySelector("[name=message]");
+
+function onLoad(e) {
+    const formData = {
+        email: email.value,
+        message: message.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function formText() {
+    const saveData = localStorage.getItem("feedback-form-state");
+    if (saveData) {
+        const parceSaveData = JSON.parse(
+            localStorage.getItem("feedback-form-state")
+        );
+        console.log(parceSaveData);
+        email.value = parceSaveData.email;
+        message.value = parceSaveData.message;
+    }
+}
+formText();
+
+form.addEventListener("submit", submitHandler);
+
+function submitHandler(e) {
+    e.preventDefault();
+    const resultObject = JSON.parse(
+        localStorage.getItem("feedback-form-state")
+    );
+    console.log(resultObject);
+    e.target.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+form.addEventListener("input", throttle(onLoad, 500));
